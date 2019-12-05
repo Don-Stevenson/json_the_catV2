24f40c4e2f32524fdbe5b97d738c5bd289999f17
Don-Stevenson <dsteven1@gmail.com>
2019-12-05 21:25:57 +0000

    working code, passing tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,21 @@
-const request = require('request')
+const request = require('request');
+const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-const url = "https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2];
-let breedName = process.argv[2];
 
-request(url, (error, response, body) => {
-   if (error) {
-    console.log('handle request errors and print the error details to the screen');
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Error, breed not found")
-  } else console.log(`The description for ${breedName} is: ${data[0].description}`);
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(url + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (!error) {
+      if (data.length === 0) {
+        callback(null, `${breedName} not found`);
+      } else {
+        callback(null, data[0].description.trim());
+      }
+    } else {
+      callback(error);
+    }
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
